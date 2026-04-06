bbdb58e11b9feb398961625154821941d565da41
Nicole Marie Botin <botinnicolemarie08@gmail.com>
2026-04-07 00:42:56 +0800

    last

--- server/index.js
+++ server/index.js
@@ -1,19 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const { connectToMongoDB } = require("./database");
-const path = require("path");
 
 const app = express();
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("Backend server is running 🚀");
+});
+
 const router = require("./routes");
 app.use("/api", router);
-
-app.use(express.static(path.join(__dirname, "../client/build")));
-
-app.get(/^(?!\/api).+/, (req, res) => {
-  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-});
 
 const port = process.env.PORT || 5000;
 
@@ -21,7 +18,7 @@ const startServer = async () => {
   try {
     await connectToMongoDB();
     app.listen(port, () => {
-      console.log(`Server is listening on http://localhost:${port}`);
+      console.log(`Server is listening on port ${port}`);
     });
   } catch (error) {
     console.error("Failed to start server:", error);
