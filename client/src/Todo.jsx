a739e013d035d18a9a197f4d2238f1f9201c04d3
Nicole Marie Botin <botinnicolemarie08@gmail.com>
2026-04-07 00:51:09 +0800

    last fix

--- client/src/Todo.jsx
+++ client/src/Todo.jsx
@@ -1,57 +1,58 @@
+const API_URL = process.env.REACT_APP_API_URL || "http://localhost:5000";
+
 export default function Todo(props) {
-    
-    const updateTodo = async (todoId, todoStatus) => {
-        const res = await fetch(`/api/todos/${todoId}`, {
-            method: "PUT",
-            body: JSON.stringify({ status: !todoStatus }), 
-            headers: {
-                "Content-Type": "application/json",
-            },
-        });
-        const json = await res.json();
+  const updateTodo = async (todoId, todoStatus) => {
+    const res = await fetch(`${API_URL}/api/todos/${todoId}`, {
+      method: "PUT",
+      body: JSON.stringify({ status: !todoStatus }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    const json = await res.json();
 
-        if (json.acknowledged) {
-            props.setTodos(currentTodos => {
-                return currentTodos.map((currentTodo) => {
-                    if (currentTodo._id === todoId) {
-                        return { ...currentTodo, status: !currentTodo.status };
-                    }
-                    return currentTodo;
-                });
-            });
-        }
-    };
+    if (json.acknowledged) {
+      props.setTodos((currentTodos) =>
+        currentTodos.map((currentTodo) => {
+          if (currentTodo._id === todoId) {
+            return { ...currentTodo, status: !currentTodo.status };
+          }
+          return currentTodo;
+        })
+      );
+    }
+  };
 
-    const deleteTodo = async (todoId) => {
-        const res = await fetch(`/api/todos/${todoId}`, {
-            method: "DELETE"
-        });
-        const json = await res.json();
-        
-        if (json.acknowledged) {
-            props.setTodos(currentTodos => {
-                return currentTodos.filter((currentTodo) => (currentTodo._id !== todoId));
-            });
-        }
-    };
+  const deleteTodo = async (todoId) => {
+    const res = await fetch(`${API_URL}/api/todos/${todoId}`, {
+      method: "DELETE",
+    });
+    const json = await res.json();
 
-    return (
-        <div className="todo">
-            <p>{props.todo.todo}</p>
-            <div className="mutations">
-                <button
-                    className="todo__status"
-                    onClick={() => updateTodo(props.todo._id, props.todo.status)}
-                >
-                    {(props.todo.status) ? "☑" : "☐"}
-                </button>
-                <button
-                    className="todo__delete"
-                    onClick={() => deleteTodo(props.todo._id)}
-                >
-                    🗑️
-                </button>
-            </div>
-        </div>
-    );
+    if (json.acknowledged) {
+      props.setTodos((currentTodos) =>
+        currentTodos.filter((currentTodo) => currentTodo._id !== todoId)
+      );
+    }
+  };
+
+  return (
+    <div className="todo">
+      <p>{props.todo.todo}</p>
+      <div className="mutations">
+        <button
+          className="todo__status"
+          onClick={() => updateTodo(props.todo._id, props.todo.status)}
+        >
+          {props.todo.status ? "☑" : "☐"}
+        </button>
+        <button
+          className="todo__delete"
+          onClick={() => deleteTodo(props.todo._id)}
+        >
+          🗑️
+        </button>
+      </div>
+    </div>
+  );
 }
--- server/index.js
+++ server/index.js
@@ -1,8 +1,17 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
 const { connectToMongoDB } = require("./database");
 
 const app = express();
+
+app.use(cors({
+  origin: [
+    "http://localhost:3000",
+    "https://awesometodosapp-1-sugj.onrender.com/"
+  ]
+}));
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
