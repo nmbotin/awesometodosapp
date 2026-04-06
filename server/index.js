e41acb867ffb702049e0a931edbe63fb5a7e3996
Nicole Marie Botin <botinnicolemarie08@gmail.com>
2026-04-06 23:30:48 +0800

    fixed database connection2

--- server/index.js
+++ server/index.js
@@ -1,29 +1,31 @@
 require("dotenv").config();
 const express = require("express");
 const { connectToMongoDB } = require("./database");
-const path = require('path');
+const path = require("path");
 
 const app = express();
 app.use(express.json());
 
-
-app.use(express.static(path.join(__dirname, 'dist')));
-
-
 const router = require("./routes");
 app.use("/api", router);
 
+app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get(/^(?!\/api).+/, (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, "../client/build", "index.html"));
 });
 
 const port = process.env.PORT || 5000;
 
 const startServer = async () => {
+  try {
     await connectToMongoDB();
     app.listen(port, () => {
-        console.log(`Server is listening on port ${port}`);
+      console.log(`Server is listening on port ${port}`);
     });
+  } catch (error) {
+    console.error("Failed to start server:", error);
+  }
 };
+
 startServer();
